d46926463fd8e858d53952b47733133f4fc85871
Krutinis <krutinis@yandex.ru>
2022-11-15 12:28:17 +0300

    add Zadacha_68

---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Функция работает только с положительными значениями!");
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Значение функции Аккермана для A({M},{N}) = {Ackerman(M,N)}");
+
+int Ackerman(int M, int N){
+    if (M == 0) return N + 1;
+    if (M > 0 && N == 0) return Ackerman(M - 1, 1);
+    if (M > 0 && N > 0) return Ackerman(M - 1, Ackerman(M, N - 1));
+    return Ackerman(M, N);
+}
